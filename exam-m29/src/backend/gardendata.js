8f9a7b1d4bfb586bb06770bf6ef94b041e5c08be
borsikapiros <73432795+borsikapiros@users.noreply.github.com>
2021-03-29 10:18:52 +0200

    json created

---
+++ exam-m29/src/backend/gardendata.js
@@ -0,0 +1,19 @@
+const gardenData = [
+    {
+        "fák": [{ "név": "cseresznye", "virágzás ideje": "április", "virág színe": "rózsaszín", "virág illata" : "semmilyen"},
+        { "név": "szilva", "virágzás ideje": "április", "virág színe": "rózsaszín", "virág illata" : "illatos"},
+        { "név": "mandula", "virágzás ideje": "március", "virág színe": "fehér", "virág illata" : "illatos"},
+        { "név": "alma", "virágzás ideje": "május", "virág színe": "rózsaszín", "virág illata" : "illatos"}]
+    }, {
+        "bokrok": [{ "név": "hóbogyó", "virágzás ideje": "november", "virág színe": "fehér", "virág illata": "semmilyen" },
+            { "név": "varjúháj", "virágzás ideje": "április", "virág színe": "sárga", "virág illata": "büdös" },
+            { "név": "orgona", "virágzás ideje": "május", "virág színe": "lila", "virág illata": "isteni" },
+            { "név": "tűztövis", "virágzás ideje": "április", "virág színe": "piros", "virág illata" : "semmilyen"}]
+    },
+    {
+        "viragok": [{ "név": "hóvirág", "virágzás ideje": "február", "virág színe": "fehér", "virág illata": "büdös" },
+            { "név": "jácint", "virágzás ideje": "március", "virág színe": "lila", "virág illata": "isteni" },
+            { "név": "nárcisz", "virágzás ideje": "április", "virág színe": "sárga", "virág illata": "semmilyen" },
+            { "név": "tulipán", "virágzás ideje": "április", "virág színe": "piros", "virág illata" : "semmilyen"}]
+    }
+]
