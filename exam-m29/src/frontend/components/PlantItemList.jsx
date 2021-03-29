5b2af494f5d6d7de95ab17842c5599fb8404274b
borsikapiros <73432795+borsikapiros@users.noreply.github.com>
2021-03-29 15:31:53 +0200

    searching half-baked

--- exam-m29/src/frontend/components/PlantItemList.jsx
+++ exam-m29/src/frontend/components/PlantItemList.jsx
@@ -1,10 +1,38 @@
+import { useEffect, useState } from "react";
 import PlantItem from "./PlantItem";
 
 export default function PlantItemList(props) {
     const plantType = props.plantType;
     const species = props.species;
 
-    const renderedSpeciesArray = species.map((specimen) => {
+    const [monthSearchTerm, setMonthSearchTerm] = useState("");
+    const [colorSearchTerm, setColorSearchTerm] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
+
+    const handleMonthChange = (event) => {
+        setMonthSearchTerm(event.target.value);
+    };
+    const handleColorChange = (event) => {
+        setColorSearchTerm(event.target.value);
+    };
+
+    function filterSpecies(species) {
+        if (
+            species.month === monthSearchTerm &&
+            species.color === colorSearchTerm
+        )
+            return true;
+    }
+
+    useEffect(
+        (filterSpecies, species) => {
+            const resultArray = species.filter(filterSpecies);
+            setSearchResults(resultArray);
+        },
+        [monthSearchTerm, colorSearchTerm]
+    );
+
+    const renderedSpeciesArray = searchResults.map((specimen) => {
         return <PlantItem specimen={specimen} />;
     });
 
@@ -18,6 +46,30 @@ export default function PlantItemList(props) {
                 Itt láthatod a {plantType} listáját. Tudsz szűrni közöttük virág
                 színe és virágzás ideje alapján:
             </p>
+            <div className="search-container">
+                <div className="search">
+                    <label htmlFor="month">
+                        Válaszd ki a virágzás hónapját:
+                    </label>
+                    <input
+                        type="text"
+                        placeholder="Kezdj el gépelni"
+                        value={monthSearchTerm}
+                        onChange={handleMonthChange}
+                    ></input>
+                </div>
+                <div className="search">
+                    <label htmlFor="color">
+                        Válaszd ki, milyen színű virágra vagy kíváncsi:
+                    </label>
+                    <input
+                        type="text"
+                        placeholder="Kezdj el gépelni"
+                        value={colorSearchTerm}
+                        onChange={handleColorChange}
+                    ></input>
+                </div>
+            </div>
             <ul>{renderedSpeciesArray}</ul>
         </div>
     );
